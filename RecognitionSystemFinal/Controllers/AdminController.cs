dd00c90d944c72d693ce40b8b7c3783d074752d4
Kevin <Kevin@LAPTOP-O3IOMVJ6>
2022-02-21 10:31:46 +0800

    Add project files.

---
+++ RecognitionSystemFinal/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecognitionSystemFinal.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+
+        public IActionResult NotificationPage()
+        {
+            return View();
+        }
+
+    }
+}
